606343be866bf1a80bb1777050ae4242c6a2e901
AdhamFA <Adhamalsalah1@gmail.com>
2023-02-28 09:04:25 -0500

    Added table repos and context to API via DI

--- rad-bank-api/rad-bank-api/Program.cs
+++ rad-bank-api/rad-bank-api/Program.cs
@@ -1,4 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Data.DB.Context;
+using Data.DB.Interfaces;
+using Data.DB.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .Build();
 
 // Add services to the container.
 
@@ -6,6 +16,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IAccountRepository, AccountRepository>();
+builder.Services.AddDbContext<RADContext>(options => options.UseSqlite(configuration["ConnectionStrings:DefaultConnection"]));
 
 var app = builder.Build();
 
@@ -23,4 +36,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
